70b371626b6a50531cf6a6b68d3eb2be742d81a8
Maksym Mamontov <madf@madf.info>
2020-11-16 14:39:19 +0200

    Add Doxygen documentation.

---
+++ docs/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['parsekey6_5',['parseKey6',['../keylib_8h.html#aafa74cfa2b74f1e01663912b549b48af',1,'keylib.h']]]
+];
---
+++ docs/search/functions_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['readkey6_6',['readKey6',['../keylib_8h.html#a3d1724884e0502cbb7c6d5b0496c4fb4',1,'keylib.h']]],
+  ['readkey6_5fbio_7',['readKey6_bio',['../keylib_8h.html#a116d1ca6e05c5c87f34abd473abb7f24',1,'keylib.h']]]
+];
